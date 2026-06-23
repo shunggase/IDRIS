def6a55d6e5bf920bcb69b902dab94268ed1ef20
shunggase <shunggase@gmail.com>
2026-06-24 01:07:08 +0700

    fix: update navigation logic and token protocols in nav.php

--- nav.php
+++ nav.php
@@ -5,17 +5,6 @@ require_once('LineLogin.php');
 
 <div class="container">
     <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom"> 
-            <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none"> 
-                Line Login Website
-            </a> 
-   
-        <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-            <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li> 
-            <li><a href="#" class="nav-link px-2">Features</a></li> 
-            <li><a href="#" class="nav-link px-2">Pricing</a></li> 
-            <li><a href="#" class="nav-link px-2">FAQs</a></li> 
-            <li><a href="#" class="nav-link px-2">About</a></li> 
-        </ul>
 
         <div class="col-md-3 text-end">
             <?php
